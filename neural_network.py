b1d08f38f65a08e33d9d1dfdb5d5c7f753a7986c
Carl Ward <carlcward@gmail.com>
2013-04-16 00:39:41 -0400

    neural_network, feed forward, basical constructors for random weights, cleaned up some code, and commented

--- neural_network.py
+++ neural_network.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python
+
+from node import Node
+
+class Neural_Network:
+	
+	hidden_nodes = []
+	output_nodes = []	
+	
+	def __init__(self):
+		self.hidden_nodes = []
+		self.output_nodes = []
+	'''
+	static constructor for a neural network with random weigts from -0.1 to 0.1
+	'''
+	@staticmethod
+	def createWithRandomWeights(i_count, h_count, o_count):
+		nn = Neural_Network()
+		for i in range(h_count):
+			nn.hidden_nodes.append( Node.initWithRandomWeights(i_count) )
+		for i in range(o_count):
+			nn.output_nodes.append( Node.initWithRandomWeights(h_count) )
+		return nn
+		
+		
+		
+	'''
+  feed the input values forward, and return the result
+  note: a + [x] is list concatenation in python
+	'''
+	def feed_forward(self, input):
+		hidden_values = reduce( lambda a,b: a + [b.calculate_value(input)], self.hidden_nodes, list() ) 
+		output_values = reduce( lambda a,b: a + [b.calculate_value(hidden_values)], self.output_nodes, list() ) 
+		return output_values
+	
+'''
+TESTING
+'''
+nn = Neural_Network.createWithRandomWeights(3,20,6)		
+print nn.feed_forward([1,2,3])
+	
--- node.py
+++ node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from math import exp
+from random import randint
 class Node:
 	#the list of weights to be applied to the input
 	weights = []
@@ -10,25 +11,48 @@ class Node:
 			self.weights = []
 		else:
 			self.weights = ws
-			
-	# the sigmoid		
+	
+	'''
+	static constructor for a node with random weights from -0.1 to 0.1
+	'''
+	@staticmethod		
+	def initWithRandomWeights(length):
+		ws = []
+		for i in range(length):
+			ws.append( (randint(0,20) - 10.0) / 100.0 )
+		return Node(ws)
+		
+	# the sigmoid	'rounds' if numbers are egregious
 	def sigmoid (self, x):
-		return 1/(1+exp(-x))
+		if x > 100:
+			return 1
+		elif x < -100:
+			return 0
+		return 1.0/(1.0+exp(-x))
 	
+	'''
+	calculate the activation value of the node given the input vector (and store it)
+	'''
 	def calculate_value (self, input):
+		if len(input) == len(self.weights):
+			def weight_sum (a,b):
+				#take the first item off and put it on the back
+				w = self.weights.pop(0)
+				self.weights.append(w)
+				#sum with that weight * the corresponding input vector
+				return a + b*w
+			cv = reduce(weight_sum, input, 0)
+			#store the calculated value in the node instance
+			self.calculated_value = self.sigmoid(cv)
+
+			return self.calculated_value
+		else:
+			raise Exception("Too many inputs given")
 	
-		def weight_sum (a,b):
-			#take the first item off and put it on the back
-			w = self.weights.pop(0)
-			self.weights.append(w)
-			#sum with that weight * the corresponding input vector
-			return a + b*w
-				
-		cv = reduce(weight_sum, input, 0)
 		
-		self.calculated_value = self.sigmoid(cv)
-
-		return self.calculated_value
-		
-
+'''
+TESTING
+'''
+n = Node([-.2,-.1,0,.1,.2,.234,1.3])
+test_inputs = [10,20,4,10,2,3,1]
 
