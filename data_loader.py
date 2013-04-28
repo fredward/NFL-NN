a696bec3359123193475d01fdd0bf9865e2d7cbf
Carl Ward <carlcward@gmail.com>
2013-04-27 23:28:59 -0400

    fixed up data loaded

--- data_loader.py
+++ data_loader.py
@@ -9,49 +9,52 @@ TODO:
 
 class Data_Loader:
 
-	def __init__(self, year):
-		self.class_dict = {}
-		self.inputs = []
-		self.targets = []
-		if year == None:
-			self.year = random.randrange(1970, 2012)
-		else:
-			self.year = year
+	def __init__(self):
+		self.year_dict = {}
 		# load the list of input vectors for a given year
 		# year is in the 2nd column of the csv file'''	
 		datafile = open('scaledData.csv', 'rU')
-		#datareader = csv.reader(datafile, dialect=csv.excel_tab)
+		
+		#skip first line
+		datafile.readline()
 		for row in datafile:
 			data = row.strip().split(',')
 			#print "Row :" + str(data) + "\n"
 			#print "Year: " + str(year) + "\ndata[1] :" + str(data[1]) + "\n"
-			if (data[1] == str(year)):
-				#print "SAME!\n"
-				del data[0]
-				del data[0]
-				#print "Row :" + str(data) + "\n"
+			
+			#print "SAME!\n"
+			(team,year) = (data.pop(0),int(data.pop(0)))
+			#print (year,team)
+			if year in self.year_dict:
 				data = [int(float(x)) for x in data]
 				output = [0, 0, 0, 0, 0, 0]
 				classif = data.pop(0)
 				output[classif] = 1
-				self.class_dict.setdefault(classif, []).append(data)
-				self.targets.append(output)
-				self.inputs.append(data)
+				self.year_dict[year].setdefault(classif,[]).append(((year,team),data))
+			else:
+				self.year_dict[year] = {}
+				data = [int(float(x)) for x in data]
+				output = [0, 0, 0, 0, 0, 0]
+				classif = data.pop(0)
+				output[classif] = 1
+				self.year_dict[year].setdefault(classif,[]).append(((year,team),data))
 		
+			
+			
+	'''
+	helper, converts classification number to output encoding 
+	i.e. 1-5 to [0,0,0,0,0,1] etc.
+	'''
+	def encode(self, classification):
+		o = [0,0,0,0,0,0]
+		o[classification] = 1
+		return o 
 
 	'''
 	returns a 'balanced' list of inputs/targets. IE 1 non-playoff, 1 wc, 1 div, 1 conf, 1 super, 1 champ
 	'''
 	def getBalancedTargets(self):
-		i = []
-		t = []
-		for classif,data in self.class_dict.items():
-			num = random.randint(0,len(data)-1)
-			i.append(data[num])
-			_o = [0,0,0,0,0,0]
-			_o[classif] = 1
-			t.append(_o)
-		return i,t
+		return None
 				
 	
 	
@@ -69,3 +72,5 @@ class Data_Loader:
 		DL = Data_Loader(year)
 				
 		return DL
+		
+dl = Data_Loader()
