d969a3bc31e61f3af2963f0d041008a5648d05b0
Carl Ward <carlcward@gmail.com>
2013-04-29 12:41:37 -0400

    comments

--- neural_main.py
+++ neural_main.py
@@ -30,7 +30,7 @@ import argparse
 
 
 def train(args):
-	#try:
+	try:
 		nn = Neural_Network.createWithRandomWeights(66,args.nodes,6)
 		inputs = []
 		targets = []
@@ -46,9 +46,10 @@ def train(args):
 				i,t = dl.getTargets(y)
 			inputs += i
 			targets += t
+		#train NN with the given data
 		nn = nn.train(args.epochs,inputs,targets,args.learn_rate)
 		nn.saveToFile(args.file)
-	#except Exception as e:
+	except Exception as e:
 		print "invalid formatting, consult neural_main.py t --help \n Error: %s" % e
 
 def predict(args):
@@ -64,6 +65,10 @@ def predict(args):
 parser = argparse.ArgumentParser(description='Predict NFL playoff results with OR train a neural network on NFL results')
 
 subparsers = parser.add_subparsers(help='Possible Actions')
+
+'''
+Create a parser for the train command, specified with 't'
+'''
 parser_train = subparsers.add_parser('t',help='train a NN over start-end years for e epochs')
 parser_train.add_argument('nodes',type=int,help='number of hidden nodes')
 parser_train.add_argument('db',type=str,help='data balancing method, u: unbalanced, o: oversampled, b:undersampled, s: SMOTE (not implemented)',choices="uobs")
@@ -72,12 +77,17 @@ parser_train.add_argument('end',type=int,help='ending year')
 parser_train.add_argument('epochs',type=int,help='number of epochs to train for')
 parser_train.add_argument('learn_rate',type=float,help='the NN learning rate')
 parser_train.add_argument('file',type=str,help='NN output file')
+# set the function to be called when this parses is used
 parser_train.set_defaults(func=train)
 
+'''
+Create a parser for the predict command, specified with 'p'
+'''
 parser_pred = subparsers.add_parser('p',help='predict the given team\'s based on the given NN')
 parser_pred.add_argument('team',type=str,help='team to predict')
 parser_pred.add_argument('year',type=int,help='year to predict')
 parser_pred.add_argument('file',type=str,help='NN file to load')
+# set the function to be called when this parser is used
 parser_pred.set_defaults(func=predict)
 
 args = parser.parse_args()
