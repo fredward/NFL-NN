f98bbdb930c028d5bc7aca7a30b5ccb898ee4d8a
Carl Ward <carlcward@gmail.com>
2013-05-03 13:15:44 -0400

    stuff

--- neural_main.py
+++ neural_main.py
@@ -36,7 +36,7 @@ def train(args):
 			i,t = dl.getBalancedTargets(y)
 		elif(args.db == 'p'):
 			dl = Data_Loader('playoffTeams.csv')
-			i,t = dl.getBalancedTargets(y)
+			i,t = dl.getTargets(y)
 		elif(args.db == 'o'):
 			dl = Data_Loader('balancedData.csv')
 			i,t = dl.getTargets(y)
@@ -44,7 +44,6 @@ def train(args):
 			dl = Data_Loader()
 			print 'Creating SMOTE targets...'
 			i,t = dl.getSmoteTargets(y)
-		
 		inputs += i
 		targets += t
 		#train NN with the given data
@@ -52,7 +51,7 @@ def train(args):
 		nn = nn.train(args.epochs,inputs,targets,args.learn_rate)
 		nn.saveToFile(args.file)
 		print "Neural Network saved to %s" % (args.file)
-	#except Exception as e:
+	except Exception as e:
 		print "invalid formatting, consult neural_main.py t --help \n Error: %s" % e
 
 def predict(args):
@@ -60,7 +59,7 @@ def predict(args):
 		nn = Neural_Network.createFromFile(args.file)
 		dl = Data_Loader()
 		#team = dl.getTeam(args.team, args.year)
-		teams = dl.getTeams(args.team, args.year)
+		team = dl.getTeam(args.team, args.year)
 		print "RESULTS: %s \n EXPECTED: %s" % (nn.feed_forward(team.stats), dl.encode(team.classification))
 		post_processor = NFL_Predictor(nn)
 		similar_teams = post_processor.compareWithPastTeams(dl.getEveryTeam(), team, 15)
@@ -70,7 +69,7 @@ def predict(args):
 		print "invalid formatting, consult neural_main.py t --help \n Error: %s" % e
 
 def cross_validate(args):
-	try:	
+	#try:	
 		nn = Neural_Network.createFromFile(args.file)
 		totalCorrect = 0
 		for y in range(args.start,args.end+1):
@@ -87,6 +86,7 @@ def cross_validate(args):
 			elif(args.db == 'o'):
 				dl = Data_Loader('balancedData.csv')
 				teams = dl.getAllTeams(y)
+			print "testing %i teams from %i" % (len(teams),y)
 			for t in teams:
 				t.result = nn.feed_forward(t.stats)
 				max_index = max(izip(t.result, count()))[1] 
@@ -100,7 +100,7 @@ def cross_validate(args):
 			print "%d \t %d" % (y, correct)
 			totalCorrect += correct
 		print "totalCorrect: %d" % totalCorrect
-	except Exception as e:
+	#except Exception as e:
 		print "invalid formatting, consult neural_main.py c --help \nError: %s" % e
 
 
@@ -113,7 +113,7 @@ Create a parser for the train command, specified with 't'
 '''
 parser_train = subparsers.add_parser('t',help='train a NN over start-end years for e epochs')
 parser_train.add_argument('nodes',type=int,help='number of hidden nodes')
-parser_train.add_argument('db',type=str,help='data balancing method, u: unbalanced, o: oversampled, b:undersampled, p:playoffTeams, s: SMOTE (not implemented)',choices="uobps")
+parser_train.add_argument('db',type=str,help='data balancing method, u: unbalanced, o: oversampled, b:undersampled, p:playoffTeams, s: SMOTE',choices="uobps")
 parser_train.add_argument('start',type=int,help='starting year to train on')
 parser_train.add_argument('end',type=int,help='ending year')
 parser_train.add_argument('epochs',type=int,help='number of epochs to train for')
@@ -128,7 +128,7 @@ Create a parser for the cross_validate command, specified with 'c'
 parser_cross = subparsers.add_parser('c',help='compare predicted outcomes with actual outcomes')
 parser_cross.add_argument('start',type=int,help='starting year to train on')
 parser_cross.add_argument('end',type=int,help='ending year')
-parser_cross.add_argument('db',type=str,help='data balancing method, u: unbalanced, o: oversampled, b:undersampled, p:playoffTeams, s: SMOTE (not implemented)',choices="uobps")
+parser_cross.add_argument('db',type=str,help='data balancing method, u: unbalanced, o: oversampled, b:undersampled, p:playoffTeams',choices="uobp")
 parser_cross.add_argument('file',type=str,help='NN input file')
 # set the function to be called when this parses is used
 parser_cross.set_defaults(func=cross_validate)
